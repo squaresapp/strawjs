fd90ce307724c728815f3c176e571079bea91773
paulgo <spam@heropaul.com>
2023-12-27 22:44:52 -0500

    Make Straw a global

--- core/X.ts
+++ core/X.ts
@@ -11,4 +11,7 @@ namespace Straw
 	const g = globalThis as any;
 	g.Fila = require("fila-core").Fila;
 	require("fila-node").FilaNode.use();
+	
+	// Straw needs to be a global.
+	g.Straw = Straw;
 }
